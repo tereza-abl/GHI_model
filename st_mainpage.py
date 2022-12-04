01ac41ad10b877d60393aae4af4da80a3c1befcc
tereza-a <terezaabelova@gmail.com>
2022-12-04 13:14:07 +0100

    add comments

--- st_mainpage.py
+++ st_mainpage.py
@@ -1,8 +1,6 @@
-import numpy as np
 import pandas as pd
 import xarray as xr
 import plotly.express as px
-import datetime
 import streamlit as st
 
 
@@ -34,6 +32,7 @@ def figure_2D(da_model, datetime_idx, lead_time_idx):
 
 def main_app(DS):
 
+    # Main config
     st.set_page_config(layout="wide")
     st.header('GHI Forecasting Model Comparison')
     st.write(f'''
@@ -45,21 +44,20 @@ def main_app(DS):
              ''')
     st.sidebar.subheader('Select data for 2D analysis')
     
+    # Retrieve values
     datetime_idx_start = DS.observed.reference_time[0].values
     datetime_idx_end = DS.observed.reference_time[-1].values
     lead_time_values = DS.model_1.lead_time.values
     latitude_values = DS.model_1.latitude.values
     longitude_values = DS.model_1.longitude.values
     
+    # Inputs from user
     date = st.sidebar.date_input(
         'Date:',
         value=pd.to_datetime(datetime_idx_start),
         min_value=pd.to_datetime(datetime_idx_start),
         max_value=pd.to_datetime(datetime_idx_end),
         )
-    # times = np.array([datetime.datetime.combine(date, datetime.time()) 
-    #                   + datetime.timedelta(minutes = i*10*5 + 20) 
-    #                   for i in range(29)])
     times = pd.to_datetime(DS.observed.reference_time.values).to_pydatetime()
     datetime_idx = st.sidebar.selectbox(
         'Time:', 
@@ -92,12 +90,14 @@ def main_app(DS):
         key='1D'
         )
 
+    # 2D visualizetion for both models
     col1, col2 = st.columns(2)
     col1.subheader('Model 1')
     col2.subheader('Model 2')
     col1.plotly_chart(figure_2D(DS.model_1, datetime_idx, lead_time_idx))
     col2.plotly_chart(figure_2D(DS.model_2, datetime_idx, lead_time_idx))
     
+    # Calculate indicators
     if pd.to_datetime(datetime_idx + pd.to_timedelta(lead_time_idx)) > datetime_idx_end:
         st.warning('No data available, choose shorter lead time.')
     else:
@@ -135,8 +135,6 @@ def main_app(DS):
             delta_color="inverse"
             )
     
-    
-    
     # 1D analysis
     st.markdown("""---""")
     st.subheader('1D Timeseries Analysis')
@@ -156,15 +154,18 @@ def main_app(DS):
                                 longitude=lon, 
                                 method='nearest').to_series()
 
+    # Create pandas dataframe
     df = pd.DataFrame({'observed':series_ob,
                        'model1':series_m1,
                        'model2':series_m2})
     
+    # Timeseries visualization
     fig_1D = px.line(df, markers=True, labels=dict(value="GHI [W/m2]"))
     fig_1D.update_layout(hovermode="x unified", width=900, height=500)
     fig_1D.update_traces(hovertemplate='%{y:.1f} [W/m2]', marker_size=5)
     st.plotly_chart(fig_1D)
     
+    # Calculate indicators
     df = df.dropna()
     col1, col2 = st.columns(2)
     kpi1, kpi2, kpi3, kpi4 = st.columns(4)
